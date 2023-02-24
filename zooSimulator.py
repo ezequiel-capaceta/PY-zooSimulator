ba8d0799d2362387224922e3191ca17e490e6141
Ezequiel Capaceta <ezequielcapaceta@zeki.local>
2023-02-23 22:00:36 -0800

    final commit

--- zooSimulator.py
+++ zooSimulator.py
@@ -28,3 +28,36 @@ class Zoo:
     self.customers.remove(name)
     print(f"{name} has left {self.name}")
   
+  def visit_animals(self):
+    for a in self.animals:
+      print(f"visiting {a.get_name()}")
+      a.make_noise()
+      a.eat_food()
+
+class Animal:
+  def __init__(self,name):
+    self.name = name
+  def get_name(self):
+    return self.name
+  def make_noise(self):
+    print("Every animal makes noise")
+  def eat_food(self):
+    print("All creatures need sustenance")
+
+nycZoo = Zoo("NYC Zoo")
+
+salmon = Fish("salmon")
+robin = Bird("robin")
+bonobo = Chimp("bonobo")
+nycZoo.add_animals([salmon, robin, bonobo])
+
+alice = Customer("Alice",25)
+bob = Customer("Bob",20)
+charlie = Customer("Charlie",10)
+dave = Customer("Dave",30)
+for c in [alice, bob, charlie, dave]:
+  c.buy_ticket()
+  c.enter_zoo(nycZoo)
+nycZoo.visit_animals()
+for c in [alice, bob, charlie, dave]:
+  c.exit_zoo(nycZoo)
